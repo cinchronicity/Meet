81f7f035b85265707dd55f1db7661322c83e8f9a
Cindy <168139730+cinchronicity@users.noreply.github.com>
2025-01-06 17:45:34 -0800

    integration testing and add google api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import './App.css'
-import EventList from './components/EventList';
-import CitySearch from './components/CitySearch';
-import NumberOfEvents from './components/NumberOfEvents';
+import React from "react";
+import "./App.css";
+import EventList from "./components/EventList";
+import CitySearch from "./components/CitySearch";
+import NumberOfEvents from "./components/NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-      <div>
-        <CitySearch />
-        <EventList />
-        <NumberOfEvents />
-      </div>
-  );
-}
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
 
-export default App
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
+
+  return (
+    <div className="App">
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <EventList events={events} />
+      <NumberOfEvents />
+    </div>
+  );
+};
+
+export default App;
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CitySearch from "../components/CitySearch";
 import { getEvents, extractLocations } from "../api";
+import App from "../App";
 
 describe("<CitySearch /> component", () => {
   let CitySearchComponent;
@@ -56,7 +57,7 @@ describe("<CitySearch /> component", () => {
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
 
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setCurrentCity={() => { }} />);
     const cityTextBox = CitySearchComponent.queryByRole("textbox");
     await user.type(cityTextBox, "Berlin");
 
@@ -68,4 +69,26 @@ describe("<CitySearch /> component", () => {
     expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
   });
   //change the list of events on suggestion that user has selected
+});
+describe('<CitySearch /> integration', () => {
+  test('renders suggestions list when the app is rendered.', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+ 
+ 
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const cityTextBox = within(CitySearchDOM).queryByRole('textbox');
+    await user.click(cityTextBox);
+ 
+ 
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+ 
+ 
+    const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem');
+    expect(suggestionListItems.length).toBe(allLocations.length + 1);
+ });
+ 
+
 });
