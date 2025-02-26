288222abd26e57600c53f90415d59a0355884e9d
Cindy <168139730+cinchronicity@users.noreply.github.com>
2025-02-26 13:13:04 -0800

    added atatus spa

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import * as atatus from 'atatus-spa';
+atatus.config('6f73cce076064e6d870ceb5cbbfa9002').install();
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+
+atatus.config('6f73cce076064e6d870ceb5cbbfa9002').install();
+atatus.notify(new Error('Test Atatus Setup'));
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
