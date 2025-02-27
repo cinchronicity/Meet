94f1d31b2eb26f07ffadbeec1701a0f747983aee
Cindy <168139730+cinchronicity@users.noreply.github.com>
2025-02-27 12:31:39 -0800

    remove atatus test setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import * as atatus from 'atatus-spa';
 
 atatus.config('6f73cce076064e6d870ceb5cbbfa9002').install();
-atatus.notify(new Error('Test Atatus Setup'));
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
