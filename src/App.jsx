fe114f40409935e99f480835cdc5b34006ff6278
Cindy <168139730+cinchronicity@users.noreply.github.com>
2025-03-03 23:19:24 -0800

    implement alert component

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,48 @@ import CitySearch from "./components/CitySearch";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { getEvents, extractLocations } from "./api";
 import { useEffect, useState } from "react";
+import { InfoAlert, ErrorAlert } from "./components/Alert";
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState(""); // new state for alert message text
+  const [errorAlert, setErrorAlert] = useState(""); // new state for error alert message text
 
   useEffect(() => {
     fetchData();
   }, [currentCity, currentNOE]);
 
   const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents =
-      currentCity === "See all cities"
-        ? allEvents
-        : allEvents.filter((event) => event.location === currentCity);
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  };
+    try {
+      const allEvents = await getEvents();
+      const filteredEvents =
+        currentCity === "See all cities"
+          ? allEvents
+          : allEvents.filter((event) => event.location === currentCity);
+      setEvents(filteredEvents.slice(0, currentNOE));
+      setAllLocations(extractLocations(allEvents));
+    } catch (error) {
+      setErrorAlert("Failed to fetch events. Please try again later.");
+    }
+  }; // new catch block to handle network or server errors
 
   return (
     <div className="App">
       <div className="App-header">
-        <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+        <div className="alerts-container">
+          {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+          {errorAlert.length > 0 && <ErrorAlert text={errorAlert} />}
+
+        </div>
+        <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
 
         <CitySearch
           allLocations={allLocations}
           setCurrentCity={setCurrentCity}
+          setInfoAlert={setInfoAlert}
         />
       </div>
       <EventList events={events} />
